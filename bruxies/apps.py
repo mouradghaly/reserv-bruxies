d61c256bef3f32352e6239416de1bca701911972
mouradghaly <101068633+mouradghaly@users.noreply.github.com>
2024-06-09 23:43:24 +0300

    Update apps.py

--- bruxies/apps.py
+++ bruxies/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
-import scheduler
 
 
 class BruxiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bruxies'
-
-class AppNameConfig(AppConfig):
-    name = 'bruxies'
-    def ready(self):
-        scheduler.start()
